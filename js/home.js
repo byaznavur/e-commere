4c7bfde004e355216854187d38ea26832c55f207
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-11-26 18:51:28 +0500

    built in layaut

--- js/home.js
+++ js/home.js
@@ -1,35 +1,29 @@
-const discountProducts = document.querySelector(".discount-products");
-const discountBtn = document.querySelector(".discountBtn");
-// Mahsulotni HTML formatida qaytaruvchi funksiya
-function discountProduct(el) {
-  return `   
-       <div class="card">
-            <div class="card-img">
-              <span class="heart"><i class="bx bxs-heart"></i></span>
-              <img src="${el.images[0]}" alt="ad" />
-              <h5 class="discount">${el.discount}%</h5>
-            </div>
-            <div class="card-name">
-              <h3><span>${el.id}.</span>${el.name}</h3>
-              <p>${el.category}</p>
-            </div>
+const bxMenu = document.querySelector(".burger");
+const catalogList = document.querySelector(".catalog-list");
+const categoryBtn = document.querySelector(".categoryBtn");
+bxMenu.addEventListener("click", () => {
+  console.log("Hello ");
+});
 
-            <div class="card-price">
-              <p>Price: <span>${el.price}$</span></p>
-              <p>Discount: <span>${el.discount}%</span></p>
-              <p>Rating: <span>${el.rating}</span></p>
-            </div>
-            <p class="card-desc">${el.description.slice(0, 40)}...</p>
+categoryBtn.addEventListener("click", () => {
+  if (catalogList.style.display == "grid") {
+    catalogList.style.display = "none";
+  } else {
+    catalogList.style.display = "grid";
+  }
+});
+bxMenu.addEventListener("click", () => {
+  if (catalogList.style.display == "grid") {
+    catalogList.style.display = "none";
+  } else {
+    catalogList.style.display = "grid";
+  }
+});
 
-            <button class="card-btn">В корзину</button>
-          </div>
-          `;
+function getCategories(el) {
+  return `<li>${el}</li>`;
 }
 
-// Faqat chegirma bor mahsulotlarni chiqarish
-products.slice(0, 5).forEach((el) => {
-  if (el.discount > 0) {
-    // Karta HTMLini yaratib qo'shamiz
-    discountProducts.innerHTML += discountProduct(el);
-  }
+categories.map((el) => {
+  catalogList.innerHTML += getCategories(el);
 });
